f9cdce0018bc0e95cefd3026bc549f3cdaf0cb9b
Eric Yan <ericyan1234@gmail.com>
2018-09-24 21:59:50 -0400

    Login screen

--- app/src/main/java/com/example/caitlinyang/m4/LoginActivity.java
+++ app/src/main/java/com/example/caitlinyang/m4/LoginActivity.java
@@ -1,24 +1,39 @@
 package com.example.caitlinyang.m4;
 
+import android.graphics.Color;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        String username = ((EditText) findViewById(R.id.usernameInput)).getText().toString().trim().toLowerCase();
-        String password = ((EditText) findViewById(R.id.passwordInput)).getText().toString();
+        username = (EditText) findViewById(R.id.usernameInput);
+        password = (EditText) findViewById(R.id.passwordInput);
         Button loginButton = (Button) findViewById(R.id.loginAccess);
 
-
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().trim().toLowerCase().equals("user") && password.getText().toString().equals("pass")) {
+                    Intent main = new Intent(getBaseContext(), ApplicationActivity.class);
+                    startActivity(main);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         // Cancel Button goes back to the welcome screen if cancel is hit
         Button cancelButton = (Button) findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(new View.OnClickListener() {

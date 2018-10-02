35b32cdbd7dc9a23431f3971f000eb591364401b
caitlinyang <hlic_yang@yahoo.com>
2018-10-01 20:23:32 -0400

    create spinner for user types

--- app/src/main/java/com/example/caitlinyang/m4/RegistrationActivity.java
+++ app/src/main/java/com/example/caitlinyang/m4/RegistrationActivity.java
@@ -4,25 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class RegistrationActivity extends AppCompatActivity{
+
+    private Spinner userTypeSpinner;
+    public static List<String> userTypes = Arrays.asList("User", "Location Employee", "Admin");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        userTypeSpinner = (Spinner) findViewById(R.id.registrationusertype);
+        // user type spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, userTypes);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        userTypeSpinner.setAdapter(adapter);
 
-//        Button submitButton = (Button) findViewById(R.id.submitRegistration);
-//        submitButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-
-
+        // cancel button going back to Welcome Screen
         Button cancelButton = (Button) findViewById(R.id.cancelRegistration);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -30,5 +35,6 @@ public class RegistrationActivity extends AppCompatActivity{
                 startActivity(welcome);
             }
         });
+
     }
 }

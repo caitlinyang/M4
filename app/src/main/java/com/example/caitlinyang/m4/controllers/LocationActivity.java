04c431195ffb259c3daffa749c40f23f114c416b
Francis Kim <frankiekim5@gmail.com>
2018-10-13 19:18:39 -0400

    Added the detail screen for each location.

--- app/src/main/java/com/example/caitlinyang/m4/controllers/LocationActivity.java
+++ app/src/main/java/com/example/caitlinyang/m4/controllers/LocationActivity.java
@@ -2,6 +2,7 @@ package com.example.caitlinyang.m4.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -12,6 +13,7 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.caitlinyang.m4.R;
+import com.example.caitlinyang.m4.model.Locations;
 import com.example.caitlinyang.m4.model.SimpleModel;
 
 import org.w3c.dom.Text;
@@ -36,7 +38,7 @@ public class LocationActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 5;
+            return 1;
         }
 
         @Override
@@ -51,24 +53,23 @@ public class LocationActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = getLayoutInflater().inflate(R.layout.individual_location_layout, null);
+            convertView = getLayoutInflater().inflate(R.layout.individual_location_layout, parent, false);
 
             TextView locName = (TextView) convertView.findViewById(R.id.textView_ind_location_name);
             TextView locType = (TextView) convertView.findViewById(R.id.textView_ind_location_type);
-            EditText lon = (EditText) convertView.findViewById(R.id.editText_ind_location_longitude);
-            EditText lat = (EditText) convertView.findViewById(R.id.editText_ind_location_latitude);
             TextView address = (TextView) convertView.findViewById(R.id.textView_ind_location_address);
             TextView phoneNum = (TextView) convertView.findViewById(R.id.textView_ind_location_phone_number);
 
-            String lonVar = Double.toString(Double.parseDouble(lon.getText().toString()));
-            String latVar = Double.toString(Double.parseDouble(lat.getText().toString()));
+            TextView lonText = (TextView) convertView.findViewById(R.id.textView_ind_location_longitude);
+            TextView latText = (TextView) convertView.findViewById(R.id.textView_ind_location_latitude);
 
-            locName.setText(SimpleModel.getInstance().getSelectedInstance().getItems().get(position).getLocationName());
-            locType.setText(SimpleModel.getInstance().getSelectedInstance().getItems().get(position).getLocationType());
-            lon.setText(lonVar);
-            lat.setText(latVar);
-            address.setText(SimpleModel.getInstance().getSelectedInstance().getItems().get(position).getAddress());
-            phoneNum.setText(SimpleModel.getInstance().getSelectedInstance().getItems().get(position).getPhoneNumber());
+
+            locName.setText(Locations.getInstance().getLocationName());
+            locType.setText("Location Type: " + Locations.getInstance().getLocationType());
+            lonText.setText("Longitude: " + Double.toString(Locations.getInstance().getLongitude()));
+            latText.setText("Latitude: " + Double.toString(Locations.getInstance().getLatitude()));
+            address.setText("Address: " + Locations.getInstance().getAddress());
+            phoneNum.setText("Phone Number: " + Locations.getInstance().getPhoneNumber());
 
             return convertView;
         }
